6b25d1766f29e30be22a004cbf8f5c120ed50c10
Federico Farina <fedefarina.melli@gmail.com>
2024-11-19 10:03:13 -0300

    Base Sudoku

---
+++ SUDOKU/main.py
@@ -0,0 +1,34 @@
+import pygame
+
+# Inicializamos pygame
+pygame.init() 
+
+
+# Tamaños y ubicaciones: X, Y 
+ancho_pantalla = 800
+largo_pantalla = 600
+dimension_pantalla = (ancho_pantalla, largo_pantalla)
+
+# Creacion de una pantalla con sus dimensiones
+pantalla = pygame.display.set_mode(dimension_pantalla) 
+
+# Cambia el titulo del juego
+pygame.display.set_caption("Sudoku") 
+
+
+juego_corriendo = True
+
+while juego_corriendo == True:  
+
+    # (Iteramos sobre todos los eventos del juego)
+    lista_eventos = pygame.event.get() 
+    for evento in lista_eventos:
+        if evento.type == pygame.QUIT: # Preguntamos si el usuario apreto la cruz para cerrar el juego.
+            juego_corriendo = False # B
+            pygame.quit() # Cerramos el juego.
+            quit()
+            
+
+    pantalla.fill((118, 182, 184)) # Llena la pantalla de un color de fondo.
+    
+    pygame.display.flip() # Siempre actualiza toda la pantalla (Siempre al final del bucle)
